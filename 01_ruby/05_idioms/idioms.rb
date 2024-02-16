8a04fb7918b74f45d043a1a7d356d0ff98c293da
Teruya Himaki <teruya.himaki@teruya.himaki>
2024-02-16 17:14:23 +0900

    feat: 05_idiomsを回答

--- 01_ruby/05_idioms/idioms.rb
+++ 01_ruby/05_idioms/idioms.rb
@@ -5,3 +5,14 @@
 #   戻り値は、引数に渡した文字列を二つ連結したものを返してください e.g. "text" => "texttext"
 #   ただし、文字列 "test" を引数に渡したときに限って、次の例外を発生させてください
 #   例外は、StandardErrorを継承したDoubleErrorを発生させてください。例外クラスは、 `Idioms::` の下の名前空間で定義してください
+
+class Idioms
+  class DoubleError < StandardError; end
+
+  def self.double(arg)
+    if arg == "test" then
+      raise DoubleError
+    end
+    arg.to_s * 2
+  end
+end
