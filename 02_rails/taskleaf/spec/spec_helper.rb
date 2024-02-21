09817fdea11f8974e6f966ab358f956eb2c8983e
Teruya Himaki <sibainu.dqx@gmail.com>
2024-02-21 14:28:42 +0900

    chore: RSpecにCapybaraの設定

--- 02_rails/taskleaf/spec/spec_helper.rb
+++ 02_rails/taskleaf/spec/spec_helper.rb
@@ -13,7 +13,13 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'capybara/rspec'
+
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
