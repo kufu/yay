bbdb9fdcb5afb6e19d0abcfd46f0d95b36ef513e
Teruya Himaki <sibainu.dqx@gmail.com>
2024-02-21 15:33:59 +0900

    test: tasksのシステムテストを追加

---
+++ 02_rails/taskleaf/spec/system/tasks_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成しておく
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
+
+    context 'ユーザーAがログインしている時' do
+      before do
+        # ユーザーAでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 作成済みのタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしている時' do
+      before do
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+        visit login_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+end
+
