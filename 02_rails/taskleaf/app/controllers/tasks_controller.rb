68c90ba62bb2fa51fadeefcb4e1b40fb22975f57
Teruya Himaki <sibainu.dqx@gmail.com>
2024-02-22 14:25:46 +0900

    feat: 画像添付機能を追加

--- 02_rails/taskleaf/app/controllers/tasks_controller.rb
+++ 02_rails/taskleaf/app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
--- 02_rails/taskleaf/app/models/task.rb
+++ 02_rails/taskleaf/app/models/task.rb
@@ -4,6 +4,7 @@ class Task < ApplicationRecord
   validate :validate_name_not_including_comma
 
   belongs_to :user
+  has_one_attached :image
 
   scope :recent, -> { order(created_at: :desc) }
 
