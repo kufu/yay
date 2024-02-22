b835e71fdc60e9e325cd46563c0e0076923b23a6
Teruya Himaki <sibainu.dqx@gmail.com>
2024-02-22 12:25:34 +0900

    feat: 入力後確認を作成

--- 02_rails/taskleaf/app/controllers/tasks_controller.rb
+++ 02_rails/taskleaf/app/controllers/tasks_controller.rb
@@ -12,8 +12,18 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
 
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
--- 02_rails/taskleaf/config/routes.rb
+++ 02_rails/taskleaf/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :users
   end
   root to: "tasks#index"
-  resources :tasks
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
